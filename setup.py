5f185c57699377346feb6ae3fee8df7d0f51f4ad
Wolfgang Schnerring <wosc@wosc.de>
2018-11-07 17:58:06 +0100

    Make current-setuptools compatible

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 """Viewing scales metrics from Pyramid"""
 from setuptools import setup, find_packages
 import glob
-import os.path
-
-
-def project_path(*names):
-    return os.path.join(os.path.dirname(__file__), *names)
 
 
 setup(
@@ -32,7 +27,7 @@ setup(
 
     keywords='',
     description=__doc__.strip(),
-    long_description='\n\n'.join(open(project_path(name)).read() for name in (
+    long_description='\n\n'.join(open(name).read() for name in (
         'README.txt',
         'HACKING.txt',
         'CHANGES.txt',
@@ -41,6 +36,6 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
-    data_files=[('', glob.glob(project_path('*.txt')))],
+    data_files=[('', glob.glob('*.txt'))],
     zip_safe=False,
 )
